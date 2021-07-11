1069cd04d8e3a84e21b73a8bba1bac40bc16878a
andrmiw9 <andreymiw@yahoo.com>
2021-07-11 20:24:43 +0300

    Нормальное движение

--- Player/Camera2D.gd
+++ Player/Camera2D.gd
@@ -1,11 +1,11 @@
 extends Camera2D
 
-const DEAD_ZONE = 160
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		var targ = event.position - get_viewport().size * 0.5
-		if(targ.length() < DEAD_ZONE):
-			self.position = Vector2(0,0)
-		else:
-			self.position = targ.normalized() * (targ.length() - DEAD_ZONE) * 0.5
+#const DEAD_ZONE = 160
+#
+#func _input(event: InputEvent) -> void:
+#	if event is InputEventMouseMotion:
+#		var targ = event.position - get_viewport().size * 0.5
+#		if(targ.length() < DEAD_ZONE):
+#			self.position = Vector2(0,0)
+#		else:
+#			self.position = targ.normalized() * (targ.length() - DEAD_ZONE) * 0.5
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-const step : int = 20		# размер шага
+#PLAYER
+
+const step : int = 500		# размер шага
 #var itemsCount : int = 0	# Кол-во вещей в инвентаре
 var speed = Vector2()		# скорость (velocity)
 #var inventory = {}		# Лист вещей в инвентаре
@@ -8,55 +10,64 @@ const jumpF = 500
 const gravity = 10
 const Rocket = preload("res://Player/Rocket.tscn")
 var direction = 1
-var isInJump = false
-const movem_koef : float = 1.5
+var wasJump = false
+#const movem_koef : float = 1.5
 
 func _physics_process(_delta):
-	if is_on_floor() and isInJump:
+	
+	if position.y < -500:
+		pass
+		#DEATH SCREEN
+	
+	if is_on_floor() and wasJump:
+		wasJump = false
 		if direction == 1:
 			$AnimatedSprite.play("Wakeup_right")
 		else:
 			$AnimatedSprite.play("Wakeup_left")
-		isInJump = false
-	if !(is_on_floor()):
-		if direction == 1:
-			$AnimatedSprite.play("Right_jump")
-		else:
-			$AnimatedSprite.play("Left_jump")
+	
 	if Input.is_action_pressed("Player_goleft"):
-		speed.x -= step
+		speed.x = -step
 		direction = -1
 		$AnimatedSprite.play("Left_run")
 		if sign($RocketSpawnP.position.x) == -1:
 			$RocketSpawnP.position.x *= -1
 	
 	elif Input.is_action_pressed("Player_goright"):
-		speed.x += step
+		speed.x = step
 		direction = 1
 		$AnimatedSprite.play("Right_run")
 		if sign($RocketSpawnP.position.x) == -1:
 			$RocketSpawnP.position.x *= -1
 	
+	else:
+		if(abs(speed.x) > 10):
+			if sign(speed.x) == 1:
+				speed.x -= 10
+			else:
+				speed.x += 10
+		else:
+			speed.x = 0
+	
 	if Input.is_action_pressed("Player_jump") and is_on_floor():
 		speed.y -= jumpF
-		isInJump = true
+		if direction == 1:
+			$AnimatedSprite.play("Right_jump")
+		else:
+			$AnimatedSprite.play("Left_jump")
+		wasJump = true
 	
 	if Input.is_action_just_pressed("Player_launch"):
 		var rocket = Rocket.instance()
-		
 		if sign($RocketSpawnP.position.x) == 1:
 			rocket.set_rocket_dir(direction)
 		else:
 			rocket.set_rocket_dir(direction)
-		
 		rocket.position = $RocketSpawnP.global_position
 		get_parent().add_child(rocket)
 	
 	speed.y += gravity
-	speed.x *= 0.99
 
 	speed = move_and_slide(speed, Vector2.UP)	# Плавно перемещаться
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#PLAYER
