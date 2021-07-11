20b92658edac25ea31c0f10ed2879141a5a0dc3b
andrmiw9 <andreymiw@yahoo.com>
2021-07-11 17:51:53 +0300

    Rocket direction

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,16 +7,32 @@ var speed = Vector2()			# скорость (velocity)
 const jumpF = 500
 const gravity = 10
 const Rocket = preload("res://Player/Rocket.tscn")
+var direction = 1
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("Player_goleft"):
 		speed.x -= step
+		direction = -1
+		if sign($RocketSpawnP.position.x) == -1:
+			$RocketSpawnP.position.x *= -1
+	
 	elif Input.is_action_pressed("Player_goright"):
 		speed.x += step
+		direction = 1
+		if sign($RocketSpawnP.position.x) == -1:
+			$RocketSpawnP.position.x *= -1
+	
 	if Input.is_action_pressed("Player_jump") and is_on_floor():
 		speed.y -= jumpF
+	
 	if Input.is_action_just_pressed("Player_launch"):
 		var rocket = Rocket.instance()
+		
+		if sign($RocketSpawnP.position.x) == 1:
+			rocket.set_rocket_dir(direction)
+		else:
+			rocket.set_rocket_dir(direction)
+		
 		rocket.position = $RocketSpawnP.global_position
 		get_parent().add_child(rocket)
 	
--- Player/Rocket.gd
+++ Player/Rocket.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 const SPEED = 200
 var vel = Vector2()
+var direction = 1
+
+func set_rocket_dir(_direction):
+	direction = _direction 
+	if _direction == -1:
+		$Sprite.flip_h = true
 
 func _physics_process(delta):
-	vel.x = SPEED * delta
+	vel.x = SPEED * delta * direction
 	translate(vel)
 	#animation
 
